50c34ffd65a6e0c7591e0ac548e92a5b963a9312
kares <self@kares.org>
2015-02-23 13:11:16 +0100

    use our enhanced throwable proxy when formatting + simplify `format` code

--- src/main/java/org/killbill/logging/log4j2/FilteredThrowablePatternConverter.java
+++ src/main/java/org/killbill/logging/log4j2/FilteredThrowablePatternConverter.java
@@ -36,13 +36,6 @@ public class FilteredThrowablePatternConverter extends ThrowablePatternConverter
         super("FilteredThrowable", "throwable", options);
     }
 
-    /**
-     * Gets an instance of the class.
-     *
-     * @param options pattern options, may be null.  If first element is "short",
-     *                only the first line of the throwable will be formatted.
-     * @return instance of class.
-     */
     public static FilteredThrowablePatternConverter newInstance(final String[] options) {
         return new FilteredThrowablePatternConverter(options);
     }
@@ -52,36 +45,37 @@ public class FilteredThrowablePatternConverter extends ThrowablePatternConverter
      */
     @Override
     public void format(final LogEvent event, final StringBuilder toAppendTo) {
-        ThrowableProxy proxy = null;
-        if (event instanceof Log4jLogEvent) {
-            proxy = ((Log4jLogEvent) event).getThrownProxy();
-        }
-        final Throwable throwable = event.getThrown();
-        if (throwable != null && options.anyLines()) {
-            if (proxy == null) {
-                super.format(event, toAppendTo);
-                return;
+        final Throwable thrown = event.getThrown();
+        if ( thrown != null && options.anyLines() ) {
+            EnhancedThrowableProxy proxy = null;
+            if ( event instanceof Log4jLogEvent ) {
+                proxy = new EnhancedThrowableProxy(thrown);
             }
+
+            if ( proxy == null ) {
+                super.format(event, toAppendTo); return;
+            }
+
             final String trace = proxy.getExtendedStackTraceAsString(options.getPackages());
             final int len = toAppendTo.length();
-            if (len > 0 && !Character.isWhitespace(toAppendTo.charAt(len - 1))) {
+            if ( len > 0 && ! Character.isWhitespace( toAppendTo.charAt(len - 1) ) ) {
                 toAppendTo.append(' ');
             }
-            if (!options.allLines() || !Constants.LINE_SEPARATOR.equals(options.getSeparator())) {
+            if ( ! options.allLines() || ! Constants.LINE_SEPARATOR.equals( options.getSeparator() ) ) {
                 final StringBuilder sb = new StringBuilder();
                 final String[] array = trace.split(Constants.LINE_SEPARATOR);
                 final int limit = options.minLines(array.length) - 1;
                 for (int i = 0; i <= limit; ++i) {
-                    sb.append(array[i]);
-                    if (i < limit) {
-                        sb.append(options.getSeparator());
-                    }
+                    sb.append( array[i] );
+                    if ( i < limit ) sb.append( options.getSeparator() );
                 }
-                toAppendTo.append(sb.toString());
+                toAppendTo.append(sb);
 
-            } else {
+            }
+            else {
                 toAppendTo.append(trace);
             }
         }
     }
+
 }
