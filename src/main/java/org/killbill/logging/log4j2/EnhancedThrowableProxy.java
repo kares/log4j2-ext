f4dd21117ac07424bf7db70921c46585e52e22ff
kares <self@kares.org>
2015-02-23 15:25:41 +0100

    avoid the extendedStackTrace in `hashCode()` - just use the message (faster hashing)

--- src/main/java/org/killbill/logging/log4j2/EnhancedThrowableProxy.java
+++ src/main/java/org/killbill/logging/log4j2/EnhancedThrowableProxy.java
@@ -405,7 +405,8 @@ public class EnhancedThrowableProxy implements Serializable {
         int result = 1;
         result = prime * result + (this.causeProxy == null ? 0 : this.causeProxy.hashCode());
         result = prime * result + this.commonElementCount;
-        result = prime * result + (this.extendedStackTrace == null ? 0 : Arrays.hashCode(this.extendedStackTrace));
+        // result = prime * result + (this.extendedStackTrace == null ? 0 : Arrays.hashCode(this.extendedStackTrace));
+        result = prime * result + (this.message == null ? 0 : this.message.hashCode());
         // result = prime * result + ( Arrays.hashCode(getSuppressedProxies()) );
         result = prime * result + (this.name == null ? 0 : this.name.hashCode());
         return result;
